45f51d117826db92416707883456eb43e59a571f
Alex Pap <alexpap1126@gmail.com>
2024-12-03 12:10:57 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Combined population cosmology with gwcosmo
+This repository contains the required files to recreate the analysis in the combined population model GW cosmology paper. This includes:
+
+- Job submission files
+- Python scripts to create mock data
+- Links to GWTC-3 data
+- Plots created by the analyses
+
+
+For any queries, please contact alexander.papadopoulos@ligo.org.
---
+++ sub_scripts/combined_shared.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export OMP_NUM_THREADS=1
+
+/data/wiay/postgrads/alex/gwcosmo_edited/bin/python \
+    /data/wiay/postgrads/alex/gwcosmo_edited/bin/gwcosmo_dark_siren_posterior --method sampling \
+    --posterior_samples /home/2386233p/BBH_data/posterior_samples.json \
+    --skymap /home/2386233p/BBH_data/skymaps.json \
+    --injections_path /data/wiay/GWTC3/injections/inj_SNR9_det_frame_2e6.h5 \
+    --parameter_dict /home/2386233p/dicts/combined_reduced_dict.json \
+    --LOS_catalog /data/wiay/postgrads/alex/empty_catalog_glade_128.hdf5 \
+    --redshift_evolution Madau \
+    --sampler dynesty \
+    --snr 11 \
+    --mass_model combined-population-powerlaw-gaussian-reduced \
+    --nlive 1000 \
+    --dlogz 0.1 \
+    --npool None \
+    --max_samps_per_pixel 2000 \
+    --outputfile comb_shared
